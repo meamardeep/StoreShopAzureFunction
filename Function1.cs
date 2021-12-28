9dd779e2fb17dbdd8e5bf6aa2dc0efda5434ff6a
Amardeep kumar <amardeepk@gnrgy.com>
2021-12-29 02:16:13 +0530

    added connection string

--- Function1.cs
+++ Function1.cs
@@ -1,16 +1,25 @@
-using System;
+using System.Text.Json;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Azure.Storage.Queues;
 
 namespace StoreShop.Function
 {
     public class Function1
     {
-        [FunctionName("Function1")]
-        public void Run([QueueTrigger("storeshopsmsqueue", Connection = "")]string myQueueItem, ILogger log)
+        [FunctionName("StoreShopFunction")]
+        public void Run([QueueTrigger("storeshopsmsqueue", Connection = "QueueConnection")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+
+            //string connectionString = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;";
+            //var queueServiceClient = new QueueServiceClient(connectionString);
+            //QueueClient queueClient = queueServiceClient.GetQueueClient("storeshopsmsqueue");
+            //var queueMessage = queueClient.ReceiveMessage();
+            SMS sMS = JsonSerializer.Deserialize<SMS>(myQueueItem);
+            
+            //call sms service provider
         }
     }
 }
---
+++ Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreShop.Function
+{
+    public class Model
+    {
+    }
+
+    public class SMS
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public string Phone { get; set; }
+        public bool IsSent { get; set; }
+    }
+}
